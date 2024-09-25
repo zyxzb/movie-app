fba68f339dd9e9ca27c63f021b1a0f49434f2585
Bartek Z <bartekzarzyk@gmail.com>
2024-09-25 10:48:49 +0200

    first commit

---
+++ app/_components/Table.tsx
@@ -0,0 +1,78 @@
+import Link from 'next/link';
+
+import ImageWithPlaiceholder from '@/_components/ImageWithPlaiceholder';
+import {
+  Table as TableCn,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/_components/ui/table';
+import { PLACEHOLDER_IMAGE } from '@/_lib/constants';
+
+type MovieProps = {
+  Title: string;
+  Year: string;
+  imdbID: string;
+  // Country?: string;
+  Type: string;
+  Poster: string;
+};
+
+type TableProps = {
+  data: MovieProps[];
+  totalResults: string;
+};
+
+const Table = ({ data, totalResults }: TableProps) => {
+  return (
+    <TableCn>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Poster</TableHead>
+          <TableHead className='w-[200px]'>Title</TableHead>
+          <TableHead className='text-center'>Year</TableHead>
+          <TableHead className='text-right'>Type</TableHead>
+          <TableHead className='text-right'>Details</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {data.map((item) => (
+          <TableRow key={item.imdbID}>
+            <TableCell className='w-[190px]'>
+              <ImageWithPlaiceholder
+                src={item.Poster !== 'N/A' ? item.Poster : PLACEHOLDER_IMAGE}
+                alt={item.Title}
+                width={96}
+                height={142}
+              />
+            </TableCell>
+            <TableCell className='font-bold'>{item.Title}</TableCell>
+            <TableCell className='text-center'>{item.Year}</TableCell>
+            <TableCell className='text-right capitalize'>{item.Type}</TableCell>
+            <TableCell className='text-right capitalize'>
+              <Link
+                href={`/i/${item.imdbID}`}
+                className='text-blue-500 underline underline-offset-2'
+              >
+                Read more...
+              </Link>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell colSpan={4}>Total results:</TableCell>
+          <TableCell className='text-right'>{totalResults}</TableCell>
+        </TableRow>
+      </TableFooter>
+      <TableCaption>The Open Movie Database</TableCaption>
+    </TableCn>
+  );
+};
+
+export default Table;
